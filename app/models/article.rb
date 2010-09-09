323ef1c128059beedc976da29d0029f5d2fa1ee3
Alex Duller <alexduller@gmail.com>
2010-09-09 20:20:40 +0100

    Added image attachment to articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,5 @@ class Article < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   validates_presence_of :category_id, :title, :content
+  has_attached_file :image, :styles => { :default => "202x139#", :square => "128x128#"}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100904172722) do
+ActiveRecord::Schema.define(:version => 20100909190945) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -21,6 +21,10 @@ ActiveRecord::Schema.define(:version => 20100904172722) do
     t.integer  "category_id"
     t.integer  "user_id"
     t.integer  "moderator_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "categories", :force => true do |t|

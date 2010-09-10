24b880c73d386553abadb940c01bc8e3e9906ba5
Alex Duller <alexduller@gmail.com>
2010-09-10 11:46:17 +0100

    Return from ability intialize if the user is not logged in

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,11 @@ class Ability
   def initialize(user)
     can :read, [Article]
     can :show, User
+
+    if user.nil?
+      return
+    end
+
     if user.is? :admin
       can :manage, :all
     end

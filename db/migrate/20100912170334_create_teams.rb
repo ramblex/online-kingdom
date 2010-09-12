6a46612f6d9bfbd8df67f4543f0d420ef215e34f
Alex Duller <alexduller@gmail.com>
2010-09-12 18:03:54 +0100

    Added teams model

---
+++ db/migrate/20100912170334_create_teams.rb
@@ -0,0 +1,15 @@
+class CreateTeams < ActiveRecord::Migration
+  def self.up
+    create_table :teams do |t|
+      t.string :name
+      t.string :country
+      t.string :website
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :teams
+  end
+end

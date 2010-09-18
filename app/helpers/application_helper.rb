2e7112100fa7a0e9e7c0d17132f1b0b319642cff
Alex Duller <alexduller@gmail.com>
2010-09-18 13:10:38 +0100

    Added a flag helper and used it in various places

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def flag(country_code)
+    image_tag "/javascripts/tiny_mce/plugins/flags/img/#{country_code}.gif", :class => 'flag'
+  end
 end

2ec2905ef52d304e2b57d70d2be77e6c9d415d52
Alex Duller <alexduller@gmail.com>
2010-09-18 11:15:08 +0100

    Set default order of matches

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,4 +1,6 @@
 class Match < ActiveRecord::Base
+  default_scope :order => 'created_at DESC'
+
   belongs_to :team1, :class_name => 'Team', :foreign_key => 'team1_id'
   belongs_to :team2, :class_name => 'Team', :foreign_key => 'team2_id'
   belongs_to :category

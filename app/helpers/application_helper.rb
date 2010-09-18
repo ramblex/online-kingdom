c11451b0a3976134d894eb944d6a8af281dfaa0d
Alex Duller <alexduller@gmail.com>
2010-09-18 13:57:59 +0100

    Added country select plugin

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,12 @@ module ApplicationHelper
   def flag(country_code)
     image_tag "/javascripts/tiny_mce/plugins/flags/img/#{country_code}.gif", :class => 'flag'
   end
+
+  def country_name(country_code)
+    # There's probably a better way of searching the array of countries
+    ActionView::Helpers::FormOptionsHelper::COUNTRIES.each do |country|
+      return country[0] if country[1].eql? country_code
+    end
+    return country_code
+  end
 end

fab618ca4719fb9c4f9f53e4da316e785809b4ea
Alex Duller <alexduller@gmail.com>
2010-09-21 11:02:44 +0100

    Able to store teams added to a tournament in the DB

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,5 +3,8 @@ class Event < ActiveRecord::Base
   has_many :event_teams
   belongs_to :category
 
+  accepts_nested_attributes_for :event_teams, :allow_destroy => true,
+    :reject_if => proc { |attributes| attributes.all? {|k,v| v.blank?}}
+
   has_attached_file :logo, :styles => {:default => '61x24#'}
 end
--- public/javascripts/tournament.js
+++ public/javascripts/tournament.js
@@ -1,3 +1,4 @@
+var position = 0;
 // Add a team to the knockout tree. Return true if another team slot needs to be added
 // to the tree in order to make it complete.
 // Expects tree to be in #tournament and the code for each team to be in #team-select
@@ -6,12 +7,13 @@ function add_team(round) {
   // div containing matches from current round
   var current = "#round-"+round;
   // HTML to insert for each team
-  var team_html = '<div class="team undo">'+$("#team-select").html()+'</div>';
+  var team_html = '<div class="team undo">'+$("#team-select").html()+'</div><input id="event_event_teams_attributes__position" name="event[event_teams_attributes][][position]" type="hidden" value="'+position+'">';
 
   if ($(current).length == 0) {
     $("#tournament").append('<div class="tournament-round undo" id="round-'+round+'"></div>');
   }
 
+  position += 1;
   // Here we may either need to complete a match or start another one.
   if ($(current+' .team').length % 2 != 0) {
     $(current+' .match').last().append(team_html);

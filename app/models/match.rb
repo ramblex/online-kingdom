56a274ac7f1b58d55ad2d78b11a1035d9670a1fb
Alex Duller <alexduller@gmail.com>
2010-09-25 18:03:45 +0100

    Order matches by start_date by default

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,5 +1,5 @@
 class Match < ActiveRecord::Base
-  default_scope :order => 'created_at DESC'
+  default_scope :order => 'start_date DESC'
 
   belongs_to :team1, :class_name => 'Team', :foreign_key => 'team1_id'
   belongs_to :team2, :class_name => 'Team', :foreign_key => 'team2_id'

0f39a0a6177ecba141c61b9ff7b3a8b119fe6ab7
Alex Duller <alexduller@gmail.com>
2010-09-29 13:12:04 +0100

    Fixed rake deploy task

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ namespace :deploy do
   end
 
   task :update_db do
-    run "cd /home/alexd/public_html/#{application}/current rake db:migrate RAILS_ENV=production"
+    run "cd /home/alexd/public_html/#{application}/current; rake db:migrate RAILS_ENV=production"
   end
 
   task :symlink_shared do

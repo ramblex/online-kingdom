663af083d3dfe8a78c563ab3fe98733da3ee858a
Alex Duller <alexduller@gmail.com>
2010-10-02 11:41:30 +0100

    Changed insider files path

--- app/controllers/insider_files_controller.rb
+++ app/controllers/insider_files_controller.rb
@@ -4,7 +4,9 @@ class InsiderFilesController < ApplicationController
     head(:forbidden) and return unless dl.downloadable?(current_user)
 
     path = dl.file.path(params[:style])
-    head(:bad_request) and return unless File.exist?(path) && params[:format].to_s == File.extname(path).gsub(/^\.+/, '')
+    Rails.logger.error(path)
+    head(:not_found) and return unless File.exist?(path)
+    head(:bad_request) and return unless params[:format].to_s == File.extname(path).gsub(/^\.+/, '')
 
     send_file path
   end
--- app/models/insider_file.rb
+++ app/models/insider_file.rb
@@ -1,12 +1,12 @@
 class InsiderFile < ActiveRecord::Base
   has_attached_file :file,
-                    :url => '/:class/:id/:basename-:style.:extension',
-                    :path => ':rails_root/assets/:class/:id_partition/:basename-:style.:extension'
+                    :url => '/:class/:id/:basename.:extension',
+                    :path => ':rails_root/assets/:class/:id_partition/:basename.:extension'
 
   before_post_process :image?
 
   def downloadable?(user)
-    user.is? :insider or user.is? :admin
+    user and (user.is? :insider or user.is? :admin)
   end
 
   def image?

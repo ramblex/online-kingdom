6f49cb9a104d7a0fee17600e3b46b467a854ab7e
Alex Duller <alexduller@gmail.com>
2011-02-11 22:40:10 +0000

    Reduced border radius in IE

--- public/javascripts/ie.js
+++ public/javascripts/ie.js
@@ -4,6 +4,6 @@ $(".ie-border").each(function() {
   var c = $(this).children().first().css('border-left-color');
   $(this).css('background', c);
 });
-$(".box").corner('6px');
-$(".button").corner('6px');
-$(".ie-border").corner('6px');
+$(".box").corner('4px');
+$(".button").corner('4px');
+$(".ie-border").corner('4px');

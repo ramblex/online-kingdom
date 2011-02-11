2e044db36cc8b9ced7ed31baabe9dd86b2b37311
Alex Duller <alexduller@gmail.com>
2011-02-11 22:38:56 +0000

    Tidied up ie stuff

--- public/javascripts/ie.js
+++ public/javascripts/ie.js
@@ -4,6 +4,6 @@ $(".ie-border").each(function() {
   var c = $(this).children().first().css('border-left-color');
   $(this).css('background', c);
 });
-$(".box").corner('5px');
-$(".button").corner('5px');
-$(".ie-border").corner('5px');
+$(".box").corner('6px');
+$(".button").corner('6px');
+$(".ie-border").corner('6px');

b4bbc4bc20675f24fe2a4faaae59dbe78986456e
Alex Duller <alexduller@gmail.com>
2011-02-11 22:55:47 +0000

    Added rounded corners to pagination

--- public/javascripts/ie.js
+++ public/javascripts/ie.js
@@ -1,5 +1,6 @@
 $(".button").wrap('<div class="ie-border"></div>');
 $(".box").wrap('<div class="ie-border"></div>');
+$(".pagination a").wrap('<div class="ie-border"></div>');
 $(".ie-border").each(function() {
   var c = $(this).children().first().css('border-left-color');
   $(this).css('background', c);
@@ -7,5 +8,6 @@ $(".ie-border").each(function() {
 $(".box").corner('5px');
 $(".box").parent().css('margin-bottom', '5px');
 $(".box").css('margin', '0');
+$(".pagination a").corner('5px');
 $(".button").corner('5px');
 $(".ie-border").corner('5px');

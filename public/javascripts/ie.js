d1c9b387256fa80e4a99271e048f61dd02f3f75c
Alex Duller <alexduller@gmail.com>
2011-02-11 21:50:20 +0000

    Another attempt at IE borders

--- public/javascripts/ie.js
+++ public/javascripts/ie.js
@@ -1,2 +1,8 @@
-$(".box").corner("5px keep");
-$(".button").corner("5px keep");
+$(".box").corner("5px").wrap('<div class="ie-border" />');
+$(".button").corner("5px").wrap('<div class="ie-border" />');
+$(".ie-border:first-child").each(function() {
+  var border_colour = $(this).css('border-left-color');
+  alert(border_colour);
+  $(this).parent().css('background-color', border_colour);
+});
+$(".ie-border").css('padding', '1px').corner('5px');

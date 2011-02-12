49ba731f6df42918a9aa16bba8b6254ca6cd685b
Alex Duller <alexduller@gmail.com>
2011-02-12 01:03:45 +0000

    Made all pagination links links

--- lib/ok_pagination.rb
+++ lib/ok_pagination.rb
@@ -2,8 +2,8 @@ class OkPagination < WillPaginate::LinkRenderer
   def to_html
     links = @options[:page_links] ? windowed_links : []
 
-    links.unshift(page_link_or_span(@collection.previous_page, 'previous', '&laquo;'))
-    links.push(page_link_or_span(@collection.next_page, 'next', '&raquo;'))
+    links.unshift(page_link(@collection.previous_page, '&laquo;', :class => 'button'))
+    links.push(page_link(@collection.next_page, '&raquo;', :class => 'button'))
 
     html = links.join(@options[:separator])
     @options[:container] ? @template.content_tag(:ul, html, {:class => 'switcher'}) : html
@@ -17,26 +17,17 @@ protected
     visible_page_numbers.inject [] do |links, n|
       # detect gaps:
       links << gap_marker if prev and n > prev + 1
-      links << page_link_or_span(n, (n == current_page ? 'button selected' : nil))
+      links << page_link(n, n.to_s, :class => (n == current_page ? 'button selected' : 'button'))
       prev = n
       links
     end
   end
 
-  def page_link_or_span(page, span_class, text = nil)
-    text ||= page.to_s
-    if page && page != current_page
-      page_link(page, text, :class => span_class)
-    else
-      page_span(page, text, :class => span_class)
-    end
-  end
-
   def page_link(page, text, attributes = {})
-    @template.content_tag(:li, @template.link_to(text, url_for(page), :class => 'button'), attributes)
-  end
-
-  def page_span(page, text, attributes = {})
-    @template.content_tag(:li, text, attributes)
+    if page.nil?
+      @template.content_tag(:li, @template.link_to(text, nil, attributes))
+    else
+      @template.content_tag(:li, @template.link_to(text, url_for(page), attributes))
+    end
   end
 end

81971681be5fa6ada6ecccb25826e76af7b2b508
Alex Duller <alexd@picochip.com>
2011-02-15 12:21:17 +0000

    Anyone can comment on events

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -33,7 +33,7 @@ class Ability
     end
 
     can :create, [Blog, Album, Video, Comment, Article, Message]
-    can :comment, [Article, Blog, Match]
+    can :comment, [Article, Blog, Match, Event]
     can :rate, Article
 
     if user.is? :news_writer

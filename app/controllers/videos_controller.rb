ab8ea835e71a373f2463ba3c29e75072e6935f02
Alex Duller <alexduller@gmail.com>
2011-02-20 10:29:59 +0000

    Videos: Don't require authentication for encode_notify

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,5 +1,5 @@
 class VideosController < ApplicationController
-  load_and_authorize_resource
+  before_filter :authenticate_user!, :except => [:encode_notify]
 
   def encode_notify
     video = Video.find_by_job_id(params[:job][:id].to_i)

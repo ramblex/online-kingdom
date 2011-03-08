412ff111618cdbe7cfe54f03afee174d8fc8bd3c
Alex Duller <alexduller@gmail.com>
2011-03-08 22:14:35 +0000

    Styled buttons and boxes in IE with images

--- public/javascripts/ie.js
+++ public/javascripts/ie.js
@@ -1,12 +1,7 @@
-$(".button").wrap('<div class="ie-border"></div>');
-$(".box").wrap('<div class="ie-border"></div>');
-$(".ie-border").each(function() {
-  var c = $(this).children().first().css('border-left-color');
-  $(this).css('background', c);
-});
-$(".box").corner('5px');
-$(".box").parent().css('margin-bottom', '5px');
-$(".box").css('margin', '0');
-$(".button").parent().css('float', 'left');
-$(".button").corner('5px');
-$(".ie-border").corner('5px');
+$(".box")
+  .append('<div class="top-left-corner"></div>')
+  .append('<div class="top-right-corner"></div>')
+  .append('<div class="bottom-left-corner"></div>')
+  .append('<div class="bottom-right-corner"></div>')
+  .append('<div class="clear"></div>');
+$(".button").wrapInner("<span></span>");
